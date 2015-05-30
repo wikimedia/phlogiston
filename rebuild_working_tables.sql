7dc26f0961f8cf2b810e299088f0744d876f5054
Joel Aufrecht <jaufrecht@wikimedia.org>
2015-05-29 17:02:13 -0700

    updated tables

--- rebuild_working_tables.sql
+++ rebuild_working_tables.sql
@@ -1,3 +1,6 @@
+drop table if exists phab_task_to_project;
+drop table if exists phab_transaction;
+drop table if exists phab_task;
 drop table if exists phab_column;
 drop table if exists phab_project;
 
@@ -11,6 +14,26 @@ create table phab_column (
        id int,
        phid text primary key,
        name text,
-       status text,
        project_phid text references phab_project
 );
+
+create table phab_task (
+       id int,
+       phid text primary key
+);
+
+create table phab_transaction (
+       id int,
+       phid text primary key,
+       object_phid text references phab_task,
+       transaction_type text,
+       new_value text,
+       date_modified timestamp
+);
+
+create table phab_task_to_project (
+       task_phid text references phab_task,
+       project_phid text references phab_project,
+       primary key (task_phid, project_phid)
+);
+
