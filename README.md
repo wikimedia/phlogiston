23157c4d43be6db42981abaa50da172653ec11f1
Joel Aufrecht <jaufrecht@wikimedia.org>
2015-07-02 16:28:54 -0700

    working on R reports

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # phab_task_history
+
+# generate a csv file with one row per task per day for VE-related projects, collapsing multi-project tasks to belong to just one project
+
+time python3 load_transactions_from_dump.py --report --verbose --project PHID-PROJ-ly2ydkopj6mc3byztenf,PHID-PROJ-nz5zs2camiseltyuemsj,PHID-PROJ-e5pkst3uyzpxifwwj7qb,PHID-PROJ-dafezmpv6huxg3taml24 --output phab_20150702.csv
--- report.R
+++ report.R
@@ -2,10 +2,40 @@
 
 library(dplyr)
 library(reshape2)
+library(ggplot2)
+library(scales)
 
 phab_data=read.csv("VE-report.csv")
+
+## data prep
+
 phab_summ = group_by(phab_data, Date, Project)
 phab_summ = summarize(phab_summ, point_total = sum(Points))
+phab_summ$Date <- as.Date(phab_summ$Date, "%Y-%m-%d")
+
+## adjust labels
+## http://stackoverflow.com/questions/1828742/rotating-axis-labels-in-r
+
+burnup_output=png(filename = "VE-report.png", width=1000, height=700, units="px", pointsize=12)
+
+
+ggplot(phab_summ, aes(x = Date, y = point_total, group=Project, fill=Project)) + geom_area(position='stack')
+
+
+ggplot(phab_summ, aes(x = Date, y = point_total, group=point_total, fill=Project)) + geom_area(position='stack') + scale_x_date(breaks="1 month", label=date_libraformat("%Y-%b"), limits = as.Date(c('2015-01-01', '2015-01-05')))
+
+
+ggplot(phab_summ, aes(x = Date, y = point_total, group=point_total, fill=Project)) + geom_area(position='stack')
+
 phab_summ_t = dcast(phab_summ, Date ~ Project)
+
+velocity = group_by(phab_data, Date, Status)
+
+velocity = summarize(velocity, point_total = sum(Points))
+
+velocityT = dcast(velocity, Date ~ Status)
+
 write.csv(VelocityT, file="velocity.csv")
 write.csv(phab_summ_t, file="backlog.csv")
+
+
