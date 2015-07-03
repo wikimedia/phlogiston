3424cc3352f6f69ce1fb77371b3fd5301f0df5d7
Joel Aufrecht <jaufrecht@wikimedia.org>
2015-07-02 17:14:36 -0700

    tweaks

--- report.R
+++ report.R
@@ -17,25 +17,24 @@ phab_summ$Date <- as.Date(phab_summ$Date, "%Y-%m-%d")
 ## http://stackoverflow.com/questions/1828742/rotating-axis-labels-in-r
 
 burnup_output=png(filename = "VE-report.png", width=1000, height=700, units="px", pointsize=12)
-
-
 ggplot(phab_summ, aes(x = Date, y = point_total, group=Project, fill=Project)) + geom_area(position='stack')
+dev.off()
+
+##
+## ggplot(phab_summ, aes(x = Date, y = point_total, group=point_total, fill=Project)) + geom_area(position='stack') + scale_x_date(breaks="1 month", label=date_libraformat("%Y-%b"), limits = as.Date(c('2015-01-01', '2015-01-05')))
 
 
-ggplot(phab_summ, aes(x = Date, y = point_total, group=point_total, fill=Project)) + geom_area(position='stack') + scale_x_date(breaks="1 month", label=date_libraformat("%Y-%b"), limits = as.Date(c('2015-01-01', '2015-01-05')))
+## ggplot(phab_summ, aes(x = Date, y = point_total, group=point_total, fill=Project)) + geom_area(position='stack')
 
+## phab_summ_t = dcast(phab_summ, Date ~ Project)
 
-ggplot(phab_summ, aes(x = Date, y = point_total, group=point_total, fill=Project)) + geom_area(position='stack')
+## velocity = group_by(phab_data, Date, Status)
 
-phab_summ_t = dcast(phab_summ, Date ~ Project)
+## velocity = summarize(velocity, point_total = sum(Points))
 
-velocity = group_by(phab_data, Date, Status)
+## velocityT = dcast(velocity, Date ~ Status)
 
-velocity = summarize(velocity, point_total = sum(Points))
-
-velocityT = dcast(velocity, Date ~ Status)
-
-write.csv(VelocityT, file="velocity.csv")
-write.csv(phab_summ_t, file="backlog.csv")
+## write.csv(VelocityT, file="velocity.csv")
+## write.csv(phab_summ_t, file="backlog.csv")
 
 
