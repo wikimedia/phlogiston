8138ee3b42b3d74798a98c2c4e5c0e9c52d3c9c0
Joel Aufrecht <jaufrecht@wikimedia.org>
2015-09-10 17:17:01 -0700

    adjusting script

--- batch_report.bash
+++ batch_report.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd
 rm phabricator_public.dump
 wget http://dumps.wikimedia.org/other/misc/phabricator_public.dump
-cd ~/phab_task_history
-cd phab_task_history
+source phlab/bin/activate
+cd ~/phlogiston
 python3 load_transactions_from_dump.py --load 
 python3 load_transactions_from_dump.py --reconstruct --report --project ve_source.py 
 python3 load_transactions_from_dump.py --reconstruct --report --project and_source.py 
