c50d4cb73029a9c53a8a74a714f4da0381d871af
Joel Aufrecht <jaufrecht@wikimedia.org>
2015-09-23 12:09:59 -0700

    fixed TR5 issue

--- ve_report.R
+++ ve_report.R
@@ -15,7 +15,7 @@ backlog$date <- as.Date(backlog$date, "%Y-%m-%d")
 ## manually set ordering of data
 colors = brewer.pal(9, "PuOr")
 
-backlog$category2 <- factor(backlog$category, levels = c("General Backlog", "TR5: Rich media tools", "TR4: Link editor tweaks", "TR3: Language support", "TR2: Mobile MVP", "TR1: Releases", "VisualEditor 2014/15 Q4 blockers", "VisualEditor 2014/15 Q3 blockers", "TR0: Interrupt", "Miscategorized"))
+backlog$category2 <- factor(backlog$category, levels = c("Uncategorized", "TR5: Rich media tools", "TR4: Link editor tweaks", "TR3: Language support", "TR2: Mobile MVP", "TR1: Releases", "VisualEditor 2014/15 Q4 blockers", "VisualEditor 2014/15 Q3 blockers", "TR0: Interrupt", "Miscategorized"))
 backlog_output=png(filename = "~/html/ve-backlog_burnup.png", width=2000, height=1125, units="px", pointsize=30)
 
 burnup <- read.csv("/tmp/ve_burnup.csv")
@@ -45,7 +45,7 @@ burnup_zoomed$date <- as.Date(burnup_zoomed$date, "%Y-%m-%d")
 burnup_zoomed$category2 <- 0 ## dummy colum so it fits on the same ggplot
 
 ggplot(backlog_zoomed) +
-  labs(title="VE Planned backlog (excludes Maintenance and General Backlog)", y="Story Point Total") +
+  labs(title="VE Planned backlog (excludes Maintenance and Uncategorized)", y="Story Point Total") +
   theme(text = element_text(size=30), legend.title=element_blank()) +
   geom_area(position='stack', aes(x = date, y = points, group=category2, fill=category2, order=-as.numeric(category2))) +
   scale_x_date(breaks="1 month", label=date_format("%Y-%b"), limits = as.Date(c('2015-02-01', NA))) +
@@ -128,13 +128,13 @@ dev.off()
 ## TR5
 
 burnup_output <- png(filename = "~/html/ve-tranch5_burnup.png", width=2000, height=1125, units="px", pointsize=30)
-ggplot(backlog[backlog$category=='TR5: ????',]) + 
+ggplot(backlog[backlog$category=='TR5: Rich media tools',]) + 
    labs(title="TR5: Rich media tools", y="Story Point Total") +
    theme(text = element_text(size=30), legend.title=element_blank())+
    geom_area(position='stack', aes(x = date, y = points, ymin=0), fill="#E08214") +
    scale_x_date(breaks="1 month", label=date_format("%Y-%b-%d"), limits = as.Date(c('2015-06-18', NA))) +
    scale_y_continuous(limits=c(0, 1000)) +
-   geom_line(data=burnup_cat[burnup_cat$category=='TR5: ????',], aes(x=date, y=points), size=2)
+   geom_line(data=burnup_cat[burnup_cat$category=='TR5: Rich media tools',], aes(x=date, y=points), size=2)
 dev.off()
 
 ######################################################################
@@ -269,7 +269,7 @@ dev.off()
 ## backlogage$week <- as.Date(backlogage$week, "%Y-%m-%d")
 ## backlogage_output <- png(filename = "~/html/ve-age_of_backlog_specific.png", width=2000, height=1125, units="px", pointsize=30)
 ## ggplot(backlogage, aes(x=week, y=points, fill=age)) +
-##   labs(title="Age of VE backlog (open tasks), excluding General Backlog", y="Story Point Total") +
+##   labs(title="Age of VE backlog (open tasks), excluding Uncategorized", y="Story Point Total") +
 ##   theme(text = element_text(size=30)) +
 ##   geom_bar(stat="identity") +
 ##   scale_fill_continuous(name="Age in months")
