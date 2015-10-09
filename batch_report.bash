71e97904d50310473f80276bddc7d4060c62ef13
Joel Aufrecht <jaufrecht@wikimedia.org>
2015-10-09 14:18:28 -0700

    batch add

--- batch_report.bash
+++ batch_report.bash
@@ -10,7 +10,10 @@ python3 load_transactions_from_dump.py --load
 # python3 load_transactions_from_dump.py --reconstruct --report --project and_source.py 
 python3 load_transactions_from_dump.py --reconstruct --report --project col_source.py
 python3 load_transactions_from_dump.py --reconstruct --report --project dis_source.py
-python3 load_transactions_from_dump.py --reconstruct --report --project dismap_source.py 
+python3 load_transactions_from_dump.py --reconstruct --report --project discir_source.py
+python3 load_transactions_from_dump.py --reconstruct --report --project diswik_source.py
+python3 load_transactions_from_dump.py --reconstruct --report --project dismap_source.py
+python3 load_transactions_from_dump.py --reconstruct --report --project fr_source.py 
 # python3 load_transactions_from_dump.py --reconstruct --report --project ios_source.py 
 python3 load_transactions_from_dump.py --reconstruct --report --project ve_source.py 
 cp /tmp/*.csv ~/html
--- fr_source.py
+++ fr_source.py
@@ -1,5 +1,5 @@
 [vars]
 prefix = fr
 default_points = 0
-project_list = Fundraising-Backlog, Fundraising Tech Backlog
+project_list = Fundraising-Backlog,Fundraising Tech Backlog
 start_date = 2015-01-01
