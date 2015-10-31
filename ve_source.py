da23ea515b44b63ee4f615a86a1cfe0b86f98a41
Joel Aufrecht <jaufrecht@wikipedia.org>
2015-10-30 21:14:01 -0700

    empty categorylist to prevent error

--- ve_source.py
+++ ve_source.py
@@ -2,6 +2,7 @@
 source_prefix = ve
 source_title = VisualEditor
 default_points = 5
+category_list = 
 project_list = VisualEditor 2014/15 Q4 blockers,VisualEditor 2014/15 Q3 blockers,VisualEditor
 start_date = 2014-12-01
 #start_date = 2015-01-07
