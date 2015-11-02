4cf64c6a9a456a36143e7f99b5486814e5c0d15f
Joel Aufrecht <jaufrecht@wikimedia.org>
2015-11-02 12:59:45 -0800

    typo

--- cot_source.py
+++ cot_source.py
@@ -3,5 +3,5 @@ source_prefix = cot
 source_title = Community-Tech
 default_points = 0
 category_list =
-project_list = Community-Tech, Community-Tech-Sprint
+project_list = Community-Tech,Community-Tech-Sprint
 start_date = 2015-01-01
