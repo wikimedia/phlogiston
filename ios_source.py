e5b33a334edafa6d14f3f1870e742d8ad17dcfc8
Joel Aufrecht <jaufrecht@mediawiki.org>
2015-11-18 16:44:03 -0800

    updated iOS for T108280 and 119028

--- ios_source.py
+++ ios_source.py
@@ -1,7 +1,7 @@
 [vars]
 source_prefix = ios
 source_title = iOS
-project_list = Wikipedia-iOS-App-Product-Backlog
+project_list = Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
 default_points = 0
 start_date=2015-01-01
 
