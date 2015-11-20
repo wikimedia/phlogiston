ef07bd9700097f2bf447a442ede858046d9ed8a3
Joel Aufrecht <jaufrecht@mediawiki.org>
2015-11-20 14:51:26 -0800

    RelEng recat

--- rel_source.py
+++ rel_source.py
@@ -2,6 +2,6 @@
 source_prefix = rel
 source_title = Release-Engineering
 default_points = 0
-project_list = RelEng-Admin, Release-Engineering-Team, releng-201516-q2, releng-201516-q3
+project_list = RelEng-Admin, Release-Engineering-Team
 start_date = 2015-01-01
 
