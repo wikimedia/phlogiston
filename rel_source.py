2e1ce3214e352528b78b8261e18eb043cbc4bf9e
Joel Aufrecht <jaufrecht@mediawiki.org>
2015-11-20 14:13:49 -0800

    initial releng attempt

--- rel_source.py
+++ rel_source.py
@@ -1,6 +1,7 @@
 [vars]
 source_prefix = rel
-source_title = 'Release Engineering'
+source_title = Release-Engineering
 default_points = 0
-project_list = Release-Engineering-Team
+project_list = RelEng-Admin, Release-Engineering-Team, releng-201516-q2, releng-201516-q3
 start_date = 2015-01-01
+
