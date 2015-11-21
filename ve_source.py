a1e6918895ff91554facfc2bdfd2c14d0ca6ba94
Joel Aufrecht <joel@aufrecht.org>
2015-11-21 12:58:32 -0800

    label change - might break things

--- ve_source.py
+++ ve_source.py
@@ -1,6 +1,6 @@
 [vars]
 source_prefix = ve
-source_title = VisualEditor
+source_title = "VisualEditor High Priority Only"
 default_points = 5
 project_list = VisualEditor 2014/15 Q4 blockers,VisualEditor 2014/15 Q3 blockers,VisualEditor
 start_date = 2015-07-01
