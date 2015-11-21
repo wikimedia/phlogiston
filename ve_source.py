a4fb5d05dae49f91b00fac388be22ce0dd64a32b
Joel Aufrecht <jaufrecht@mediawiki.org>
2015-11-20 17:58:38 -0800

    added graphs

--- ve_source.py
+++ ve_source.py
@@ -2,7 +2,7 @@
 source_prefix = ve
 source_title = VisualEditor
 default_points = 5
-project_list = VisualEditor 2014/15 Q4 blockers,VisualEditor 2014/15 Q3 blockers,VisualEditor
+project_list = VisualEditor 2014/15 Q4 blockers,VisualEditor 2014/15 Q3 blockers,VisualEditor,Graph-VisualEditor
 start_date = 2015-07-01
 
 
