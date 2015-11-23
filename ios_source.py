3ca96027ab93eb0a51ed6cec552557a451d4cbba
Joel Aufrecht <joel@aufrecht.org>
2015-11-22 23:32:55 -0800

    new start date

--- ios_source.py
+++ ios_source.py
@@ -3,7 +3,7 @@ source_prefix = ios
 source_title = iOS
 project_list = iOS-5-app-alpha,iOS-5-app-beta,iOS-5-app-production,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
 default_points = 0
-start_date=2015-01-01
+start_date=2015-08-01
 
 
 
