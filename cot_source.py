b07929b70d514d54742b7250caddb01676fad019
Joel Aufrecht <joel@aufrecht.org>
2015-12-01 15:10:20 -0800

    added new tags

--- cot_source.py
+++ cot_source.py
@@ -2,5 +2,5 @@
 source_prefix = cot
 source_title = Community-Tech
 default_points = 0
-project_list = Community-Tech-Sprint,Community-Tech
+project_list = Community-Tech-fixes,Gadgets-2.0,CopyVio-tools,WikiProject-tools,RevSlider,Community-Tech-Sprint,Community-Tech
 start_date = 2015-01-01
