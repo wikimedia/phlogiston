3fa3487347b6839b58ab4100be660c9bcaed295f
Joel Aufrecht <joel@aufrecht.org>
2015-11-30 20:21:01 -0800

    changed cutoff to when Alpha/beta/etc tags were created, as per Max B.

--- ios_source.py
+++ ios_source.py
@@ -3,8 +3,4 @@ source_prefix = ios
 source_title = iOS
 project_list = iOS-5-app-alpha,iOS-5-app-beta,iOS-5-app-production,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
 default_points = 0
-start_date=2015-08-01
-
-
-
-
+start_date=2015-09-25
