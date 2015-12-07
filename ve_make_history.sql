d592591dbbecdfc750deec39802eda04ae2b9dd3
Joel Aufrecht <jaufrecht@mediawiki.org>
2015-12-07 12:27:11 -0800

    take out priority filter

--- ve_make_history.sql
+++ ve_make_history.sql
@@ -73,7 +73,6 @@ SELECT source,
   FROM task_history
  WHERE project = 'VisualEditor'
    AND projectcolumn SIMILAR TO 'TR%'
-   AND (priority = '80' OR priority = '100')
    AND source = :'prefix'
    AND date >= '2015-06-18'
 GROUP BY status, category, date, source);
