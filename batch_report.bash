5bd78b1b792b5a2e0368875353ae62afdfc58064
Joel Aufrecht <jaufrecht@mediawiki.org>
2015-12-15 13:33:58 -0800

    initial Reading Web

--- batch_report.bash
+++ batch_report.bash
@@ -18,6 +18,7 @@ python3 phlogiston.py --reconstruct --report --project fr_source.py
 python3 phlogiston.py --reconstruct --report --project ios_source.py
 python3 phlogiston.py --reconstruct --report --project ja_source.py
 python3 phlogiston.py --reconstruct --report --project phl_source.py
+python3 phlogiston.py --reconstruct --report --project red_source.py
 python3 phlogiston.py --reconstruct --report --project rel_source.py
 python3 phlogiston.py --reconstruct --report --project tpg_source.py
 python3 phlogiston.py --reconstruct --report --project ve_source.py
