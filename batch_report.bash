f1ff5cc96451e917bf58fa6b73c67067162526d0
Joel Aufrecht <joel@aufrecht.org>
2015-12-26 17:52:38 -0500

    tidying

--- batch_report.bash
+++ batch_report.bash
@@ -22,6 +22,3 @@ python3 phlogiston.py --reconstruct --report --project red_source.py
 python3 phlogiston.py --reconstruct --report --project rel_source.py
 python3 phlogiston.py --reconstruct --report --project tpg_source.py
 python3 phlogiston.py --reconstruct --report --project ve_source.py
-
-
-
