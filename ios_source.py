38bf5ff2c588dd76d1280dba7e276889f7fd143a
Joel Aufrecht <jaufrec@users.noreply.github.com>
2016-01-14 13:42:22 -0800

    Update ios_source.py
    changed to match renamed app 5.0 tags

--- ios_source.py
+++ ios_source.py
@@ -1,6 +1,6 @@
 [vars]
 source_prefix = ios
 source_title = iOS
-project_list = iOS-5-app-alpha,iOS-5-app-beta,iOS-5-app-production,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
+project_list = iOS-app-v5-production,iOS-app-v5-beta,iOS-app-v5-alpha,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
 default_points = 0
 start_date=2015-09-25
