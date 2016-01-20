a7863b204a3a64e79e53d7a23bd761973a066d51
Joel Aufrecht <jaufrecht@mediawiki.org>
2016-01-19 16:20:15 -0800

    need to add sprint to source, ahead of other projects, to get proper title for recategorization experiment

--- and_source.py
+++ and_source.py
@@ -2,5 +2,5 @@
 source_prefix = and
 source_title = Android
 default_points = 0
-project_list = Wikipedia-Android-App,Mobile-App-Goals
+project_list = Mobile-App-Android-Sprint-74-Tungsten,Wikipedia-Android-App,Mobile-App-Goals
 start_date = 2015-07-01
