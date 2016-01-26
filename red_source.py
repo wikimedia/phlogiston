80aa2d51b20ff5f63427fe23cd6a314b02550035
Joel Aufrecht <jaufrec@users.noreply.github.com>
2016-01-26 11:57:49 -0800

    Update red_source.py

--- red_source.py
+++ red_source.py
@@ -2,6 +2,6 @@
 source_prefix = red
 source_title = Reading-Web
 default_points = 0
-project_list = Reading Web Planning,Cards,Gather,Hovercards,MediaWiki-extension-RelatedArticles,MediaWiki-extensions-MultimediaViewer,MediaWiki-Interface,MobileFrontend,PageImages,QuickSurveys,TextExtracts,Vector,Wikidata-Page-Banner
+project_list = Reading-Web-Planning,Cards,Gather,Hovercards,MediaWiki-extension-RelatedArticles,MediaWiki-extensions-MultimediaViewer,MediaWiki-Interface,MobileFrontend,PageImages,QuickSurveys,TextExtracts,Vector,Wikidata-Page-Banner
 start_date = 2015-10-01
 
