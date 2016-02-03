0585ca37772cf173dc633c28b5a676a0765db474
Joel Aufrecht <jaufrecht@mediawiki.org>
2016-02-03 14:43:24 -0800

    move start date back to fix completed work chart artifact

--- ve_source.py
+++ ve_source.py
@@ -3,5 +3,5 @@ source_prefix = ve
 source_title = "VisualEditor"
 default_points = 5
 project_list = VisualEditor 2014/15 Q4 blockers,VisualEditor 2014/15 Q3 blockers,VisualEditor
-start_date = 2015-11-15
+start_date = 2015-10-01
 
