337577e87fea64b54d1650754c3dac3b69882cc0
Joel Aufrecht <jaufrecht@mediawiki.org>
2016-02-04 16:01:17 -0800

    updated to new recat data model; changed to core/strategic

--- ve_make_history.sql
+++ ve_make_history.sql
@@ -1,14 +1,3 @@
-UPDATE task_history
-   SET status = '"open"'
- WHERE status = '"stalled"'
-   AND source = :'prefix';
-
-DELETE FROM task_history
- WHERE (status = '"duplicate"'
-    OR status = '"invalid"'
-    OR status = '"declined"')
-   AND source = :'prefix';
-
 /* ##################################################################
 Apply current work breakdown to historical data.  VE has used both
 project and projectcolumn to categorize work; this script condenses
@@ -19,95 +8,74 @@ projects called "VisualEditor 2014/15 Q3 blockers" and
 "VisualEditor 2014/15 Q4 blockers".   This query should get everything
 from those projects. */
 
-INSERT INTO tall_backlog (source, date, category, status, points, count)
+INSERT INTO task_history_recat (
 SELECT source,
        date,
+       id,
+       title,
        project as category,
        status,
-       SUM(points) as points,
-       COUNT(title) as count
+       points,
+       'Strategic'       
   FROM task_history
  WHERE project != 'VisualEditor'
-   AND source = :'prefix'
- GROUP BY status, category, date, source;
+   AND source = :'prefix');
+
 
 /* Prior to 18 June 2015, VE work in the VisualEditor project was not
 organized around the interrupt or maintenance, so all work in that
 project prior to that date can be considered Uncategorized,
-regardless of state or column.  The nested select is required to
-accurately group by category, after category is forced to a constant
-in the inner select. */
+regardless of state or column. */
 
-INSERT INTO tall_backlog (source, date, category, status, points, count) (
+INSERT INTO task_history_recat(
 SELECT source,
        date,
-       category,
-       status,
-       SUM(points) as points,
-       COUNT(title) as count
-  FROM (
-SELECT source,
-       date,
-       CAST('Uncategorized' AS text) as category,
+       id,
+       title,
+       'Uncategorized',
        status,
        points,
-       title
+       'Core'
   FROM task_history
  WHERE project = 'VisualEditor'
    AND date < '2015-06-18'
-   AND source = :'prefix') as ve_old_other
-   GROUP BY status, category, date, source);
+   AND source = :'prefix');
 
 /* Since June 18, 2018, the projectcolumn in the VisualEditor project
 should be accurate, so any VE task in a Tranche should use the tranche
-as the category. */
+as the category. Any other tasks in the VisualEditor project (i.e.,
+any task after June 18th and not in a tranche) should be old data
+getting cleaned up. */
 
-INSERT INTO tall_backlog (source, date, category, status, points, count) (
+INSERT INTO task_history_recat(
 SELECT source,
        date,
+       id,
+       title,
+       COALESCE(project,'') || ' ' ||
        COALESCE(projectcolumn,'') || ' ' ||
        COALESCE(milestone_title,'') as category,
        status,
-       SUM(points) as points,
-       COUNT(title) as count
+       points,
+       CASE WHEN projectcolumn SIMILAR TO 'TR0%' THEN 'Core'
+            WHEN projectcolumn SIMILAR TO 'TR%' THEN 'Strategic'
+            ELSE 'Core' END
   FROM task_history
  WHERE project = 'VisualEditor'
    AND projectcolumn SIMILAR TO 'TR%'
    AND source = :'prefix'
-   AND date >= '2015-06-18'
-GROUP BY status, category, date, source);
+   AND date >= '2015-06-18');
 
-/* Any other tasks in the VisualEditor project (i.e., any task after
-June 18th and not in a tranche) should be old data getting cleaned up.
-We will essentially ignore them by placing them in Uncategorized.
-*/
+/* Simplify status fields */
 
-INSERT INTO tall_backlog (source, date, category, status, points, count) (
-SELECT source,
-       date,
-       category,
-       status,
-       SUM(points) as points,
-       count(title) as count
-  FROM (
-SELECT source,
-       date,
-       CAST('Uncategorized' AS text) || ' ' ||
-       COALESCE(milestone_title,'') as category,
-       status,
-       points,
-       title
-  FROM task_history
- WHERE project = 'VisualEditor'
-   AND projectcolumn NOT SIMILAR TO 'TR%'
-   AND source = :'prefix'
-   AND date >= '2015-06-18') AS ve_new_uncategorized
-GROUP BY status, category, date, source);
+UPDATE task_history_recat
+   SET status = '"open"'
+ WHERE status = '"stalled"'
+   AND source = :'prefix';
 
-/* Set Maintenance flags, since VE uses tranches instead of tags to determine this */
+DELETE FROM task_history_recat
+ WHERE (status = '"duplicate"'
+    OR status = '"invalid"'
+    OR status = '"declined"')
+   AND source = :'prefix';
 
-UPDATE tall_backlog
-   SET maint_type = CASE WHEN category LIKE '%TR0%' THEN 'Maintenance'
-                         ELSE 'New Functionality'
-                    END
- WHERE source = :'prefix';
