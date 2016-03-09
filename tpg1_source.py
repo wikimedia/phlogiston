01cda696df9fa39973621db217135c0c4d86ea16
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-03-09 12:22:21 -0800

    experimenting with different TPG categorization scheme

---
+++ tpg1_source.py
@@ -0,0 +1,6 @@
+[vars]
+source_prefix = tpg
+source_title = Team-Practices-Group
+default_points = 0
+project_list = TPG-2016Q3,TPG-Essential-Functions,Team-Practices,Team-Practices-This-Week
+start_date = 2015-10-01
