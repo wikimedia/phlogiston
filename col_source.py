d18a99abf0f09665890da33df2670581eb7c10e0
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-03-15 16:29:54 -0700

    new Collab projects for FY2016Q4

--- col_source.py
+++ col_source.py
@@ -2,5 +2,5 @@
 source_prefix = col
 source_title = Collaboration
 default_points = 0
-project_list = Collaboration-Team-Sprint-M-2014-12-17,Collaboration-Team-Sprint-P-2015-02-11,Collaboration-Team-Sprint-Q-2015-02-25,Collaboration-Team-Sprint-R-2015-03-11,Collaboration-Team-Sprint-S-2015-03-25,Collaboration-Team-Sprint-T-2015-04-08,Collaboration-Team-Sprint-U-2015-04-22,Collaboration-Team-Sprint-A-2015-05-06,Collaboration-Team-Sprint-B-2015-05-20,Collaboration-Team-Sprint-C-2015-06-17,Collaboration-Team-Sprint-D-2015-06-30,Collaboration-Team-Sprint-E-Everywhere-2015-07-14,Collaboration-Team-Current,Collaboration-Team-Backlog,Education-Collab
+project_list = Flow,Notifications,Collaboration-Community-Engagement,Collaboration-Team-Backlog,Collaboration-Team-Current
 start_date = 2015-10-01
