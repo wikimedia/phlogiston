fd80175d89259b80e6d8f34d2a4f68f3e874c5af
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-04-12 14:21:03 -0700

    Update col_scope.py

--- col_scope.py
+++ col_scope.py
@@ -1,5 +1,7 @@
 [vars]
 scope_title = Collaboration
 default_points = 0
-project_list = Flow,Notifications,Collaboration-Community-Engagement,Collaboration-Team-Backlog,Collaboration-Team-Current
-start_date = 2015-10-01
+project_list = Collab-Team-2016-Q4,Flow,Notifications,Collaboration-Community-Engagement,Collaboration-Team-Backlog,Collaboration-Team-Current
+start_date = 2016-01-01
+retroactive_categories = True
+backlog_resolved_cutoff = 2016-04-01
