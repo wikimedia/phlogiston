80a36d6065810c2b6be19cde0f705da6696d9998
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-04-18 20:20:56 -0700

    Update col_scope.py
    adding more tags to read in for dual-tag categorization

--- col_scope.py
+++ col_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = Collaboration
 default_points = 0
-project_list = Collab-Team-2016-Q4,Notifications
+project_list = Collab-Team-2016-Q4,Notifications,Notifications-Page,Flow
 start_date = 2016-01-01
 retroactive_categories = True
 backlog_resolved_cutoff = 2016-04-01
