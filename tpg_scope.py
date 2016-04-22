72e201254e6bb7ac828f98bde569aaf57133c9fe
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-04-22 11:43:28 -0700

    reordered

--- tpg_scope.py
+++ tpg_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = Team-Practices-Group
 default_points = 0
-project_list = Team-Practices,Team-Practices-This-Week,TPG-2016Q4-Assess-Skills,TPG-2016Q4-Support-Quarterly,TPG-2016Q4-Measure-Light-Engagement,TPG-Essential-Functions,TPG-2016Q3
+project_list = TPG-2016Q4-Assess-Skills,TPG-2016Q4-Support-Quarterly,TPG-2016Q4-Measure-Light-Engagement,TPG-Essential-Functions,TPG-2016Q3,Team-Practices-This-Week,Team-Practices
 start_date = 2016-01-01
 retroactive_categories = True
 backlog_resolved_cutoff = 2016-04-01
