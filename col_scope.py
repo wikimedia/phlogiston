f17ae14aec4dbcc7429badbfc354acc77e6180c0
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-04-22 15:27:26 -0700

    fixed priority order of projects

--- col_scope.py
+++ col_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = Collaboration
 default_points = 0
-project_list = Collab-Team-2016-Q4,Notifications,Notifications-Page,Flow
+project_list = Notifications-Page,Flow,Notifications,Collab-Team-2016-Q4
 start_date = 2016-01-01
 retroactive_categories = True
 backlog_resolved_cutoff = 2016-04-01
