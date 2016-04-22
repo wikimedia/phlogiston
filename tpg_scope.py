41c6087a9c0ce329d642d0db8217a5f79485f639
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-04-22 11:07:53 -0700

    adding other projects

--- tpg_scope.py
+++ tpg_scope.py
@@ -1,7 +1,10 @@
 [vars]
 scope_title = Team-Practices-Group
 default_points = 0
-project_list = Team-Practices,Team-Practices-This-Week
+project_list = Team-Practices,Team-Practices-This-Week,TPG-2016Q4-Assess-Skills,TPG-2016Q4-Support-Quarterly,TPG-2016Q4-Measure-Light-Engagement,TPG-Essential-Functions,TPG-2016Q3
 start_date = 2016-01-01
 retroactive_categories = True
 backlog_resolved_cutoff = 2016-04-01
+
+
+
