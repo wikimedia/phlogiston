0fa53df60a5a693453a34b33674cc4046b562ea6
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-04-26 12:41:42 -0700

    Update cot_scope.py
    added backlog cutoff

--- cot_scope.py
+++ cot_scope.py
@@ -3,3 +3,4 @@ scope_title = Community-Tech
 default_points = 0
 project_list = Community-Tech-fixes,Gadgets-2.0,CopyVio-tools,WikiProject-tools,RevSlider,Community-Tech-Sprint,Community-Tech
 start_date = 2015-10-01
+backlog_resolved_cutoff = 2016-01-01
