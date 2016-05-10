e08e10ccd0058eff4dcc497689421ec3f3f6e50c
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-05-10 16:39:34 +0000

    bringing more recent

--- ja_scope.py
+++ ja_scope.py
@@ -3,4 +3,6 @@ scope_title = JoelAufrecht
 default_points = 0
 project_list = User-JAufrecht
 start_date = 2015-10-01
-backlog_resolved_cutoff = 2016-01-01
+backlog_resolved_cutoff = 2016-04-01
+show_count = True
+show_points = False
