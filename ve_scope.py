9853b0415785d3ac78e2c2505daa4ac7f1c003fc
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-06-10 08:53:02 +0000

    don't need all of 2015

--- ve_scope.py
+++ ve_scope.py
@@ -2,9 +2,9 @@
 scope_title = "VisualEditor"
 default_points = 5
 project_list = VisualEditor 2014/15 Q4 blockers,VisualEditor 2014/15 Q3 blockers,VisualEditor
-start_date = 2015-01-01
+start_date = 2016-01-01
 retroactive_categories = True
 retroactive_points = True
-backlog_resolved_cutoff = 2016-01-01
+backlog_resolved_cutoff = 2016-04-01
 show_count = False
 show_points = True
