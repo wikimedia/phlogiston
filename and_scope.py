70a42be0fb7e8b7a313d1ed495b9aab6247a5eb4
mbinder-wmf <mbinder@wikimedia.org>
2016-06-22 18:47:31 +0200

    Update and_scope.py

--- and_scope.py
+++ and_scope.py
@@ -1,5 +1,5 @@
 [vars]
 scope_title = Android
 default_points = 0
-project_list = Mobile-App-Android-Sprint-74-Tungsten,Wikipedia-Android-App-Backlog,Mobile-App-Goals
+project_list = Wikipedia-Android-App-Backlog,Mobile-App-Goals
 start_date = 2015-10-01
