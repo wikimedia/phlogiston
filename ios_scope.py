e3973d62340eaee229672e263defb03bd966f1f6
mbinder-wmf <mbinder@wikimedia.org>
2016-06-22 18:54:32 +0200

    Update ios_scope.py

--- ios_scope.py
+++ ios_scope.py
@@ -1,6 +1,6 @@
 [vars]
 scope_title = iOS
-project_list = iOS-app-v5-production,iOS-app-v5-beta,iOS-app-v5-alpha,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
+project_list = iOS-app-v5-production,iOS-app-v5-beta,iOS-app-v5-alpha,iOS-app-v5.0.1-Kiwi,iOS-app-v5.0.2-Rubiks,iOS-app-v5.0.3-Disco,iOS-app-v5.0.5-Fever,iOS-app-v5.1.0-Saturn,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
 default_points = 0
 start_date=2015-10-01
 show_points = False
