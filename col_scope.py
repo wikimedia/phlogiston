01609b4d6261b4ea94e0893671536445b87e1db1
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-06-28 23:39:49 +0000

    added FY2017-18-Q1 settings for Collab team

--- col_scope.py
+++ col_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = Collaboration
 default_points = 0
-project_list = Collab-Notifications-Page,Flow,Notifications,Collab-Team-2016-Apr-Jun-Q4
+project_list = Edit-Review-Improvements,Collab-Notifications-Page,Flow,Notifications,Collab-Team-Q1-July-Sep-2016,Collab-Team-2016-Apr-Jun-Q4
 start_date = 2016-01-01
 retroactive_categories = True
 backlog_resolved_cutoff = 2016-04-01
