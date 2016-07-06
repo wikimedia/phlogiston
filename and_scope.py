01e4d18d2b62358bed007a79f6318ea19f70482d
mbinder-wmf <mbinder@wikimedia.org>
2016-07-06 13:38:09 -0700

    Update and_scope.py

--- and_scope.py
+++ and_scope.py
@@ -1,5 +1,5 @@
 [vars]
 scope_title = Android
 default_points = 0
-project_list = Wikipedia-Android-App-Backlog,Android-app-feature-Feeds,Android-app-feature-Navigation,Android-app-feature-Navigation-MVP
+project_list = Android-app-feature-Feeds-MVP,Android-app-feature-Feeds,Android-app-feature-Navigation-MVP,Android-app-feature-Navigation,Wikipedia-Android-App-Backlog
 start_date = 2015-10-01
