f74f04f8c605cd79fd2dd661110815a66ea72f0f
mbinder-wmf <mbinder@wikimedia.org>
2016-08-05 13:39:42 -0700

    Update ios_scope.py

--- ios_scope.py
+++ ios_scope.py
@@ -3,5 +3,5 @@ scope_title = iOS
 project_list = ios-app-bugs (HockeyApp),ios-app-bugs,ios-app-feature-ipad,ios-app-feature-offline,ios-app-feature-namespaces,ios-app-feature-saved,ios-app-feature-topread,ios-app-feature-performance,ios-app-feature-wp0,ios-app-feature-toc,ios-app-feature-accessibility,ios-app-feature-analytics,ios-app-feature-links,ios-app-feature-multilingual,ios-app-feature-notifications,ios-app-feature-nearby,ios-app-feature-database,ios-app-feature-database-ios10,iOS-app-v5-production,iOS-app-v5-beta,iOS-app-v5-alpha,iOS-app-v5.0.1-Kiwi,iOS-app-v5.0.2-Rubiks,iOS-app-v5.0.3-Disco,iOS-app-v5.0.5-Fever,iOS-app-v5.1.0-Saturn,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
 default_points = 0
 start_date=2015-10-01
-show_points = False
+show_points = True
 show_count = True
