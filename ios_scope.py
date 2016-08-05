0e41e890659db6894787e81b4beb3e97ee686cee
mbinder-wmf <mbinder@wikimedia.org>
2016-08-05 14:19:13 -0700

    Update ios_scope.py

--- ios_scope.py
+++ ios_scope.py
@@ -1,6 +1,6 @@
 [vars]
 scope_title = iOS
-project_list = ios-app-bugs (HockeyApp),ios-app-bugs,ios-app-feature-ipad,ios-app-feature-offline,ios-app-feature-namespaces,ios-app-feature-saved,ios-app-feature-topread,ios-app-feature-performance,ios-app-feature-wp0,ios-app-feature-toc,ios-app-feature-accessibility,ios-app-feature-analytics,ios-app-feature-links,ios-app-feature-multilingual,ios-app-feature-notifications,ios-app-feature-nearby,ios-app-feature-database,ios-app-feature-database-ios10,iOS-app-v5-production,iOS-app-v5-beta,iOS-app-v5-alpha,iOS-app-v5.0.1-Kiwi,iOS-app-v5.0.2-Rubiks,iOS-app-v5.0.3-Disco,iOS-app-v5.0.5-Fever,iOS-app-v5.1.0-Saturn,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
+project_list = iOS-app-Bugs (HockeyApp),iOS-app-Bugs,iOS-app-feature-iPad,iOS-app-feature-Offline,iOS-app-feature-Namespaces,iOS-app-feature-Saved,iOS-app-feature-Topread,iOS-app-feature-Performance,iOS-app-feature-WP0,iOS-app-feature-TOC,iOS-app-feature-Accessibility,iOS-app-feature-Analytics,iOS-app-feature-Links,iOS-app-feature-Multilingual,iOS-app-feature-Notifications,iOS-app-feature-Nearby,iOS-app-feature-Database,iOS-app-feature-iOS10,iOS-app-v5-production,iOS-app-v5-beta,iOS-app-v5-alpha,iOS-app-v5.0.1-Kiwi,iOS-app-v5.0.2-Rubiks,iOS-app-v5.0.3-Disco,iOS-app-v5.0.5-Fever,iOS-app-v5.1.0-Saturn,iOS-app-v5.2.0-Honey,Wikipedia-iOS-App-Product-Backlog,Wikipedia-iOS-App-Development
 default_points = 0
 start_date=2015-10-01
 show_points = True
