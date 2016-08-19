a6bb38e0edbc5b246924f006024eae1cf4e28b4a
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-08-19 16:54:47 -0700

    more work in progress

---
+++ loading_functions.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION convert_blocked_phid_to_id()
+RETURNS void AS $$
+BEGIN
+
+        INSERT INTO maniphest_blocked
+        SELECT mb.blocked_date, mt1.id, mt2.id
+          FROM maniphest_blocked_phid mb,
+               maniphest_task mt1,
+               maniphest_task mt2
+         WHERE mb.blocks_phid = mt1.phid
+           AND mb.blocked_by_phid = mt2.phid;
+
+    RETURN;
+END;
+$$ LANGUAGE plpgsql;
