85b7ee4ea415b80e0831fd67411758dbce8b6827
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-08-25 23:51:47 +0000

    restore retroactive categorization, which is essential for collab's backlog

--- col_scope.py
+++ col_scope.py
@@ -3,6 +3,6 @@ scope_title = Collaboration
 default_points = 0
 project_list = Edit-Review-Improvements,Collab-Notifications-Page,Flow,Notifications,Collab-Team-Q1-July-Sep-2016,Collab-Team-2016-Apr-Jun-Q4
 start_date = 2016-04-01
-retroactive_categories = False
+retroactive_categories = True
 backlog_resolved_cutoff = 2016-07-01
 show_points = False
