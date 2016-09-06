7cc3fe50474373706877a271f928345cdf4e17ff
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-09-06 10:56:40 -0700

    update col for T143723

--- col_scope.py
+++ col_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = Collaboration
 default_points = 0
-project_list = Edit-Review-Improvements,Collab-Notifications-Page,Flow,Notifications,Collab-Team-Q1-July-Sep-2016,Collab-Team-2016-Apr-Jun-Q4
+project_list = Augmented-Changes-Feed,Edit-Review-Improvements,Collab-Notifications-Page,Flow,Notifications,Collab-Team-Q1-July-Sep-2016,Collab-Team-2016-Apr-Jun-Q4
 start_date = 2016-04-01
 retroactive_categories = False
 backlog_resolved_cutoff = 2016-07-01
