fb34434ec2dd75c8b7c5c8eaf37f3313c08c5726
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-09-16 23:22:42 +0000

    updating for new rules

--- phl_scope.py
+++ phl_scope.py
@@ -1,7 +1,6 @@
 [vars]
 scope_title = Phlogiston
 default_points = 0
-project_list = Phlogiston
-start_date = 2015-09-01
+start_date = 2016-04-01
 backlog_resolved_cutoff = 2016-07-01
 retroactive_points = True
