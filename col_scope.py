d9880b5327cab1bbdb375205931d45e0e3ec8cb0
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-12-07 18:47:52 +0000

    Updated for FY2017Q2

--- col_scope.py
+++ col_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = Collaboration
 default_points = 0
-start_date = 2016-04-01
+start_date = 2016-07-01
 retroactive_categories = True
-backlog_resolved_cutoff = 2016-07-01
+backlog_resolved_cutoff = 2016-10-01
 show_points = False
