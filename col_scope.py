9219a2b9801e48b166c7bbda3d131a58218a2b9f
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-12-07 23:22:27 +0000

    tweaking collaboration settings; adding status report experiment

--- col_scope.py
+++ col_scope.py
@@ -2,6 +2,7 @@
 scope_title = Collaboration
 default_points = 0
 start_date = 2016-07-01
-retroactive_categories = True
+retroactive_categories = False
 backlog_resolved_cutoff = 2016-10-01
 show_points = False
+status_report_range = 60
