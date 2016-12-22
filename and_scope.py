5219466404c6061a81fbe474875c2eef7f960989
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-12-22 04:07:54 +0000

    earlier start dates

--- and_scope.py
+++ and_scope.py
@@ -1,5 +1,5 @@
 [vars]
 scope_title = Android
 default_points = 0
-start_date = 2016-07-01
+start_date = 2015-07-01
 backlog_resolved_cutoff = 2016-10-01
--- ios_scope.py
+++ ios_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = iOS
 default_points = 0
-start_date = 2016-07-01
+start_date = 2015-07-01
 backlog_resolved_cutoff = 2016-10-01
 show_points = True
 show_count = True
