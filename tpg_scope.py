ff543c86bcc4de8adfa3b612b4f668180b63be3f
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-12-22 04:05:57 +0000

    update dates and categoriez

--- tpg_scope.py
+++ tpg_scope.py
@@ -1,7 +1,7 @@
 [vars]
 scope_title = Team-Practices-Group
 default_points = 0
-start_date = 2016-07-01
+start_date = 2015-07-01
 retroactive_categories = True
 backlog_resolved_cutoff = 2016-10-01
 status_report_range = 7
