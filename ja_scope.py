ecd6b8945e143b649f4d69bb92abe40acfc6e3f2
Joel Aufrecht <jaufrecht@wikimedia.org>
2016-12-22 05:45:24 +0000

    updated for earlier start and most recent status report cutoff

--- ja_scope.py
+++ ja_scope.py
@@ -2,10 +2,10 @@
 scope_title = JoelAufrecht
 default_points = 0
 project_list = User-JAufrecht
-start_date = 2016-09-01
+start_date = 2015-07-01
 retroactive_categories = False
-backlog_resolved_cutoff = 2016-10-01
-status_report_range = 4
+backlog_resolved_cutoff = default
+status_report_start = 2016-12-15
 status_report_project = 2335
 show_count = True
 show_points = False
