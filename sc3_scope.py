600d0f0848c2f78e5c0bd8ace05750551d1f8004
Joel Aufrecht <jaufrecht@wikimedia.org>
2017-01-06 22:06:21 +0000

    Scap prototype

---
+++ sc3_scope.py
@@ -0,0 +1,10 @@
+[vars]
+scope_title = Scap3
+default_points = 0
+start_date = 2015-01-01
+retroactive_categories = False
+backlog_resolved_cutoff = default
+status_report_range = default
+status_report_project = 1449
+show_points = False
+
