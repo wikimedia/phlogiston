af8199ad360609edcbd0772bbbeb814669a346e8
Joel Aufrecht <jaufrecht@wikimedia.org>
2017-01-20 19:54:49 +0000

    fix status report project for q3

--- col_scope.py
+++ col_scope.py
@@ -5,6 +5,6 @@ start_date = 2015-10-01
 retroactive_categories = False
 backlog_resolved_cutoff = default
 status_report_range = 2017-01-09
-status_report_project = 2198
+status_report_project = 2415
 show_points = False
 
