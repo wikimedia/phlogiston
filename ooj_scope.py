35ca3a1fddaac236494a6e434a6a0a5115645b0d
Joel Aufrecht <jaufrecht@wikimedia.org>
2017-04-13 22:44:27 +0000

    added new OOjs-UI project

---
+++ ooj_scope.py
@@ -0,0 +1,10 @@
+[vars]
+scope_title = OOjs-UI
+default_points = 0
+start_date = 2016-07-01
+retroactive_categories = False
+backlog_resolved_cutoff = 2017-01-01
+status_report_start = true
+status_report_project = 697
+show_points = False
+
