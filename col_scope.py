e5508b1d4ec94dbf8f640f035a9cc1f95da23c51
Joel Aufrecht <jaufrecht@wikimedia.org>
2017-04-18 20:39:04 +0000

    updated col for Q4

--- col_scope.py
+++ col_scope.py
@@ -3,8 +3,8 @@ scope_title = Collaboration
 default_points = 0
 start_date = 2015-10-01
 retroactive_categories = False
-backlog_resolved_cutoff = 2017-01-01
-status_report_start = 2017-01-09
-status_report_project = 2415
+backlog_resolved_cutoff = default
+status_report_start = 2017-04-17
+status_report_project = 2659
 show_points = False
 
