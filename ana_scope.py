7169867446a92396be563f197aba9cf149e49765
Joel Aufrecht <jaufrecht@wikimedia.org>
2017-07-07 16:29:26 +0000

    adjusted scope to match feedback

--- ana_scope.py
+++ ana_scope.py
@@ -1,5 +1,6 @@
 [vars]
 scope_title = Analytics
 default_points = 0
-start_date = 2016-04-01
+start_date = 2017-04-01
 backlog_resolved_cutoff = default
+status_report_project = 11
--- rel_scope.py
+++ rel_scope.py
@@ -2,3 +2,4 @@
 scope_title = Release-Engineering
 default_points = 0
 start_date = 2017-01-01
+status_report_project = 2769
