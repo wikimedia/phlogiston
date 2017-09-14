10247d7e52fe91a3a74f41ba97b793597c33e15c
Joel Aufrecht <jaufrecht@wikimedia.org>
2017-09-14 22:37:56 +0000

    adjusted q1 baseline date

--- col_scope.py
+++ col_scope.py
@@ -4,7 +4,7 @@ default_points = 0
 start_date = 2015-10-01
 retroactive_categories = False
 backlog_resolved_cutoff = default
-status_report_start = 2017-04-17
-status_report_project = 2659
+status_report_start = 2017-07-15
+status_report_project = 2828
 show_points = False
 
