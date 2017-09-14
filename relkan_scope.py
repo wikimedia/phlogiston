a2d262601f99a6a4ae9fa9ddfb6f632932e3f3d9
Joel Aufrecht <jaufrecht@wikimedia.org>
2017-09-14 22:36:01 +0000

    170359 review

--- relkan_scope.py
+++ relkan_scope.py
@@ -1,6 +1,6 @@
 [vars]
-scope_title = Release-Engineering-Backlog
+scope_title = Release-Engineering-Kanban
 default_points = 0
 start_date = 2017-01-01
-status_report_project = 20
+status_report_project = 2769
 status_report_start = True
