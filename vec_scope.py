2b41e9ed7dd7b48757d3cd12a7ce4cd9954ee016
Joel Aufrecht <jaufrecht@wikimedia.org>
2018-02-15 17:26:55 +0000

    added VEC scope

---
+++ vec_scope.py
@@ -0,0 +1,10 @@
+[vars]
+scope_title = "VisualEditor Current"
+default_points = 5
+start_date = 2018-02-01
+retroactive_categories = True
+retroactive_points = True
+status_report_project = 3236
+backlog_resolved_cutoff = default
+show_count = True
+show_points = True
