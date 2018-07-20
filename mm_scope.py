8cd0be985715136d3ef66828e61a2b4aded26ba2
Max Binder (WMF) <mbinder@wikimedia.org>
2018-07-20 13:18:15 -0700

    Update mm_scope.py

--- mm_scope.py
+++ mm_scope.py
@@ -2,5 +2,5 @@
 scope_title = Multimedia
 default_points = 0
 start_date = 2017-05-01
-status_report_project = 48
-status_report_start = 2017-09-07
+status_report_project = 3014
+status_report_start = 2018-01-01
