5e826e72bf96348de488849700651cbeea3451f6
Max Binder (WMF) <mbinder@wikimedia.org>
2018-07-20 15:22:22 -0700

    Update cot_scope.py

--- cot_scope.py
+++ cot_scope.py
@@ -1,6 +1,6 @@
 [vars]
 scope_title = Community-Tech
-default_points = 0
-project_list = English-Wikipedia-New-Pages-Patrol,MediaWiki-extensions-GlobalPreferences,MediaWiki-extensions-TemplateWizard,Grant-Metrics,MediaWiki-extensions-RelatedSites,MediaWiki-extensions-TranslateSvg,MediaWiki-extensions-CodeMirror,MediaWiki-extensions-LoginNotify,MediaWiki-extensions-PageAssessments,Community-Tech-Sprint,Community-Tech
+default_points = 2
 start_date = 2017-10-01
+status_report_project = 1500
 backlog_resolved_cutoff = default
