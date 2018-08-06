2b638bb4314f7ce70b970a2f15eae7cf5199d16a
Joel Aufrecht <jaufrecht@wikimedia.org>
2018-08-06 17:10:07 +0000

    fixed cloud config

--- clo_scope.py
+++ clo_scope.py
@@ -1,9 +1,7 @@
 [vars]
 scope_title = Cloud Services
 default_points = 0
-# DEBUG
-#start_date = 2017-05-18
-start_date = 2018-07-01
+start_date = 2017-05-18
 backlog_resolved_cutoff = default
 status_report_start = default
 status_report_project = 2774
