b8a37b45da708891bbea4dda3cec6e6f03cbb007
Max Binder (WMF) <mbinder@wikimedia.org>
2018-08-24 15:04:36 -0700

    Update red_scope.py

--- config/red_scope.py
+++ config/red_scope.py
@@ -3,5 +3,5 @@ scope_title = Readers-Web
 default_points = 0
 start_date = 2017-12-01
 status_report_project = 2739
-status_report_start = 2017-12-14
-status_report_end = 2018-01-10
+status_report_start = 2018-01-10
+status_report_end = 2018-01-24
